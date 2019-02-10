6ae60371de6d9abae2c19c8ba808f27ae62b9108
oliver <OliverC189@gmail.com>
2019-02-10 18:30:11 +0100

    logging improved

--- src/main/java/de/oc/kafkaplayground/listener/Listener.java
+++ src/main/java/de/oc/kafkaplayground/listener/Listener.java
@@ -2,25 +2,31 @@ package de.oc.kafkaplayground.listener;
 
 import de.oc.kafkaplayground.exception.RecoverableException;
 import de.oc.kafkaplayground.exception.UnrecoverableException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class Listener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+
     private static final String RECOVERABLE = "recoverable";
     private static final String UNRECOVERABLE = "unrecoverable";
 
 
     @KafkaListener(topics = "${kafka.topic.playground}")
-    public void receive(String payload){
-        System.out.println("Kafka Message received. (Payload: '" + payload + "')");
-        if(RECOVERABLE.equals(payload)){
+    public void receive(ConsumerRecord<String, String> record) {
+        LOGGER.info("ConsumerRecord received:\nkey: '{}'\nvalue: '{}'", record.key(), record.value());
+        if (RECOVERABLE.equals(record.value())) {
             throw new RecoverableException();
         }
-        if(UNRECOVERABLE.equals(payload)){
+        if (UNRECOVERABLE.equals(record.value())) {
             throw new UnrecoverableException();
         }
+        LOGGER.info("ConsumerRecord succesfully processed");
     }
 
 }
--- src/main/java/de/oc/kafkaplayground/listener/RecoverableDltListener.java
+++ src/main/java/de/oc/kafkaplayground/listener/RecoverableDltListener.java
@@ -1,6 +1,8 @@
 package de.oc.kafkaplayground.listener;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,12 +11,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RecoverableDltListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RecoverableDltListener.class);
 
     public static final String ID_RECOVERABLE_DLT_LISTENER = "recoverable.dlt.listener";
 
     @KafkaListener(id = ID_RECOVERABLE_DLT_LISTENER, topics = "${kafka.topic.playground}" + "${kafka.topic.deadletter.suffix.recoverable}")
     public void processDlt(ConsumerRecord<String, String> record) {
-        System.out.println("Dead Letter Record recovered, value: '" + record.value() + "'.");
+        LOGGER.info("ConsumerRecord recovered:\nkey: '{}'\nvalue: '{}'\nDLT Exception Message: {}",
+                record.key(),
+                record.value(),
+                new String(record.headers().lastHeader("kafka_dlt-exception-message").value()));
     }
 
     @Bean
@@ -22,9 +28,11 @@ public class RecoverableDltListener {
         return args -> {
             while (true) {
                 listenerRegistry.getListenerContainer(ID_RECOVERABLE_DLT_LISTENER).pause();
-                Thread.sleep(60000); // 1 min
+                LOGGER.info(ID_RECOVERABLE_DLT_LISTENER + " paused");
+                Thread.sleep(20000);
                 listenerRegistry.getListenerContainer(ID_RECOVERABLE_DLT_LISTENER).resume();
-                Thread.sleep(60000); // 1 min
+                LOGGER.info(ID_RECOVERABLE_DLT_LISTENER + " resumed");
+                Thread.sleep(20000);
             }
         };
     }
